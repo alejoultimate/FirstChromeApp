359e2c3d4990f2106d25720c58500c5745eca4f8
Alejandro Lopez <lalejandro78@gmail.com>
2016-04-26 17:36:46 -0500

    Escuchar en el puerto

--- main.js
+++ main.js
@@ -13,9 +13,18 @@ function myFunction() {
 function conectarPuerto() {
     var rutaPuertoActual = document.getElementById("listaDePuertos").value;
     var puerto = new Puerto( rutaPuertoActual );
+    //puerto.escucharPuerto();
     puerto.abrirPuerto();
-    document.getElementById('textConexion').innerHTML = "Puerto conectado";
+    document.getElementById('txtEstadoConexion').innerHTML = "Puerto conectado";
 }
+
+function escucharPuerto() {
+    var rutaPuertoActual = document.getElementById("listaDePuertos").value;
+    var puerto = new Puerto( rutaPuertoActual );
+    puerto.escucharPuerto();
+    document.getElementById('txtEscuchando').innerHTML = "Escuchando";
+}
+
 
 /* Permite cargar múltiples funciones de JavaScript en el evento onload */
 function addLoadEvent(func) {
@@ -35,9 +44,11 @@ function addLoadEvent(func) {
 
 addLoadEvent(func1);
 addLoadEvent(myFunction);
+addLoadEvent(escucharPuerto);
 
 
 document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('conectarButton').addEventListener('click', conectarPuerto);
+    document.getElementById('escucharButton').addEventListener('click', escucharPuerto);
 });
 
--- puerto.js
+++ puerto.js
@@ -1,14 +1,39 @@
   function Puerto(rutaDelPuertoSerial) {
+    this.connectionId = -1;
     this.rutaDelPuertoSerial = rutaDelPuertoSerial;
   }
 
 
   Puerto.prototype.abrirPuerto = function () {
     var onConnect = function(connectionInfo) {
-    // The serial port has been opened. Save its id to use later.
-    this.connectionId = connectionInfo.connectionId;
-    // Do whatever you need to do with the opened port.
+      // The serial port has been opened. Save its id to use later.
+      this.connectionId = connectionInfo.connectionId;
+      // Do whatever you need to do with the opened port.
     };
-  // Connect to the serial port /dev/ttyS01
-  chrome.serial.connect(this.rutaDelPuertoSerial, {bitrate: 115200}, onConnect);
+    // Connect to the serial port /dev/ttyS01
+    chrome.serial.connect(this.rutaDelPuertoSerial, {bitrate: 9600, ctsFlowControl: true}, onConnect);
+  };
+  
+  
+  
+  Puerto.prototype.escucharPuerto = function () {
+    var stringReceived = '';
+  
+    var onReceiveCallback = function(info) {
+      if (info.connectionId == this.connectionId && info.data) {
+        var str = convertArrayBufferToString(info.data);
+        if (str.charAt(str.length-1) === '\n') {
+          stringReceived += str.substring(0, str.length-1);
+          onLineReceived(stringReceived);
+          stringReceived = '';
+        } else {
+          stringReceived += str;
+        }
+      }
+    };
+
+    chrome.serial.onReceive.addListener(onReceiveCallback);
+    
+    console.log("Escuchando: " + stringReceived);
+    
   };
