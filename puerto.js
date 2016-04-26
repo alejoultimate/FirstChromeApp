ce8707328c21f9186c3f50f0993b64f45d7225ce
Alejandro Lopez <lalejandro78@gmail.com>
2016-04-26 00:19:45 -0500

    Cambios al abrir el puerto

--- puerto.js
+++ puerto.js
@@ -6,7 +6,7 @@
   Puerto.prototype.abrirPuerto = function () {
     var onConnect = function(connectionInfo) {
     // The serial port has been opened. Save its id to use later.
-    _this.connectionId = connectionInfo.connectionId;
+    this.connectionId = connectionInfo.connectionId;
     // Do whatever you need to do with the opened port.
     };
   // Connect to the serial port /dev/ttyS01
