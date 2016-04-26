178b99bbc8611d1e8bf33755781819417c49de57
Alejandro Lopez <lalejandro78@gmail.com>
2016-04-26 00:06:03 -0500

    Cambios en main.js

--- main.js
+++ main.js
@@ -10,7 +10,13 @@ function myFunction() {
     console.log("Hello! I am an alert box!");
 }
 
-
+function conectarPuerto() {
+    var puerto = new Puerto( "COM1" );
+    puerto.abrirPuerto();
+    
+    document.getElementById('demo').innerHTML = "Puerto conectado";
+      
+}
 
 /* Permite cargar múltiples funciones de JavaScript en el evento onload */
 function addLoadEvent(func) {
@@ -30,3 +36,9 @@ function addLoadEvent(func) {
 
 addLoadEvent(func1);
 addLoadEvent(myFunction);
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('conectarButton').addEventListener('click', conectarPuerto);
+});
+
