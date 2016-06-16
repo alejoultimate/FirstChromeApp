274615121cbf51f0d95d24f65c53caa7c6f646a1
alejoultimate <lalejandro78@hotmail.com>
2016-06-16 18:37:18 -0500

    Crear el driver driverfortesting.js

---
+++ driverfortesting.js
@@ -0,0 +1,19 @@
+function DriverForTesting (newConfiguration) {
+  this.setConfiguration(newConfiguration);
+}
+
+// inherits From DriverForAnalyzer
+DriverForTesting.prototype = new DriverForAnalyzer(null);
+
+DriverForTesting.prototype.driverResponse = function () {
+  // Imprimir en pantalla la configuración del driver
+  console.log(this.getConfiguration());
+  // Imprimir en pantalla el protocolo ASTM
+  console.log(this.getProtocolASTM());
+  // Respuesta automática
+  return "Respuesta automatica : " + this.getStringReceived();
+};
+
+////////////////////////////////////////
+/*  Hasta aquí "Driver para pruebas"  */
+////////////////////////////////////////
