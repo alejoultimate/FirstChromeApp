03f7d8a18d6ce96b49c5d48cebdb25c546433ada
alejoultimate <lalejandro78@hotmail.com>
2016-06-16 14:31:43 -0500

    Adicionar Jasmine para hacer TDD

--- driverforanalyzer.js
+++ driverforanalyzer.js
@@ -75,7 +75,7 @@ DriverForAnalyzer.prototype.isResponseRequired = function () {
 DriverForAnalyzer.prototype.cleanSpecialCharacters = function (data) {
   var arraySpecialCharacters =  [ 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31 ];
   var newData = "";
-  for (i = 0; i < data.length - 1; i++) {
+  for (i = 0; i < data.length; i++) {
         ch = data.charCodeAt(i);
         // Buscar el caracter en el array de caracteres especiales
         if ( arraySpecialCharacters.indexOf(ch) === -1 ) {
