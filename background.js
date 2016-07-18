c85493a691c49911a884720c429ecfd16df40184
alejoultimate <lalejandro78@hotmail.com>
2016-07-18 14:10:57 -0500

    Ajustes minimos

--- background.js
+++ background.js
@@ -5,12 +5,20 @@
  * @see http://developer.chrome.com/apps/app.window.html
  */
 
-chrome.app.runtime.onLaunched.addListener(function(launchData) {
-  chrome.app.window.create(
-    'index.html',
-    {
+chrome.app.runtime.onLaunched.addListener(function() {
+  // Center window on screen.
+  var screenWidth = screen.availWidth;
+  var screenHeight = screen.availHeight;
+  var width = 500;
+  var height = 300;
+  
+  chrome.app.window.create( 'index.html', {
       id: 'mainWindow',
-      bounds: {width: 800, height: 600}
-    }
-  );
+      outerBounds: {
+        width: width,
+        height: height,
+        left: Math.round((screenWidth-width)/2),
+        top: Math.round((screenHeight-height)/2)
+      }
+  });
 });
